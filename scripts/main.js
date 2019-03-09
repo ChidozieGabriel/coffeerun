b37a6f70b6866ffe5ce77d78fa0fb2f81fff2db7
Chidozie Gabriel <nnabugwuchidozie@gmail.com>
2019-03-09 08:23:28 +0100

    Add order validation

--- scripts/main.js
+++ scripts/main.js
@@ -6,18 +6,21 @@
 	var Truck = App.Truck;
 	var DataStore = App.DataStore;
 	var FormHandler = App.FormHandler;
+	var Validation = App.Validation;
 	var CheckList = App.CheckList;
 	var myTruck = new Truck('ncc-1701', new DataStore());
 	window.myTruck = myTruck;
-
 	var formHandler = new FormHandler(FORM_SELECTOR);
+	var checkList = new CheckList(CHECKLIST_SELECTOR);
+
 	formHandler.addSubmitHandler(function (data) {
 		myTruck.createOrder.call(myTruck, data);
 		checkList.addRow.call(checkList, data);
 	});
 
-	var checkList = new CheckList(CHECKLIST_SELECTOR);
 	checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+
+	formHandler.addInputHandler(Validation.isCompanyEmail);
 
 })(window);
 
